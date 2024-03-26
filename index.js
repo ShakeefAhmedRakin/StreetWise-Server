53fa5b8dfcbf9148d9e05e01081202543f9700cd
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-03-26 11:44:49 +0600

    Created post and get api for user creation and userinfo getting with middlewares.

--- index.js
+++ index.js
@@ -22,11 +22,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // TOKEN AUTH API
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
+    // COLLECTIONS
+    const userCollection = client
+      .db("StreetWiseDB")
+      .collection("userCollection");
 
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+    // JWT TOKEN AUTH API
+    app.post("/jwt", async (req, res) => {
+      const uid = req.body;
+
+      const token = jwt.sign(uid, process.env.ACCESS_TOKEN, {
         expiresIn: "1h",
       });
       res.send({ token });
@@ -35,17 +40,69 @@ async function run() {
     // MIDDLEWARE
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized access" });
+        return res.status(401).send({ message: "Unauthorized Access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "unauthorized access" });
+          return res.status(401).send({ message: "Unauthorized Access" });
         }
         req.decoded = decoded;
         next();
       });
     };
+
+    const verifyAdmin = async (req, res, next) => {
+      const uid = req.decoded.uid;
+      const query = { uid: uid };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+      next();
+    };
+
+    app.get("/test", verifyToken, verifyAdmin, async (req, res) => {
+      res.send("working");
+    });
+
+    // ----------------------APIS-------------------------
+
+    // USER CREATION POST API
+    app.post("/create-user", async (req, res) => {
+      try {
+        const userInfo = req.body;
+        const userExists = await userCollection.findOne({ uid: userInfo.uid });
+
+        if (!userExists) {
+          const result = await userCollection.insertOne(userInfo);
+          res.send(result);
+        } else {
+          res.send({ prevUser: true });
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    // USER INFO GET API
+    app.get("/get-user/:uid", verifyToken, async (req, res) => {
+      try {
+        const uid = req.params.uid;
+        const userInfo = await userCollection.findOne({ uid });
+
+        if (userInfo) {
+          res.send(userInfo);
+        } else {
+          res.status(404).send({ message: "User not found" });
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    // ---------------------------------------------------
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
